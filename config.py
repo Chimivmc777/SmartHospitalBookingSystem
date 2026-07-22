e4884bd86a55f3e9d2e478fb5c7a7f7fb33c02aa
Chimivmc777 <chimininja777@gmail.com>
2026-07-22 18:40:12 +0530

    Configure app for Railway deployment

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = "Chimivmc777@"
-DB_NAME = "smart_hospital"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv("DB_PORT"))
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
